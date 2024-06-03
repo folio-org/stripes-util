a1776f65efb538b41995e8dd3923bfbb33ebb118
Zak Burke <zburke@ebsco.com>
2024-06-03 10:14:52 -0400

    STUTL-45 implement escapeCqlWildcards
    Implement a new function to escape all five CQL wildcards, instead of
    just two as was done in `escapeCqlValue()`:
    * asterisk (*)
    * question mark (?)
    * caret (^) (sic)
    * backslash (\)
    * quote (")
    
    Refs STUTL-45, STUTL-33

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 export { default as effectiveCallNumber } from './lib/effectiveCallNumber';
 export { default as escapeCqlValue } from './lib/escapeCqlValue';
+export { default as escapeCqlWildcards } from './lib/escapeCqlWildcards';
 export { default as exportCsv } from './lib/exportCsv';
 export { default as getFullName } from './lib/getFullName';
 export { default as parseJwt } from './lib/parseJwt';

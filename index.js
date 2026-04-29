23ecf04f036b2c659e5a0499fde4a006e5a4af71
mariia-aloshyna <mariia_aloshyna@epam.com>
2026-04-29 16:57:40 +0300

    STUTL-62: rename function to isValidUUID

--- index.js
+++ index.js
@@ -7,6 +7,6 @@ export { default as getFullName } from './lib/getFullName';
 export { default as parseJwt } from './lib/parseJwt';
 export { default as getHeaderWithCredentials } from './lib/getHeaderWithCredentials';
 export { default as getSourceSuppressor } from './lib/getSourceSuppressor';
-export { default as validateUUID } from './lib/validateUUID';
+export { default as isValidUUID } from './lib/isValidUUID';
 export * from './validators';
 export * from './lib/permission';
